8782e8d292973996a6b4e60504a0752a41984cfe
Clarence Baluyot <akingprincess@gmail.com>
2024-12-10 21:38:46 +0800

    Add todays_ride_events

--- backend/rides/serializer.py
+++ backend/rides/serializer.py
@@ -2,6 +2,7 @@
 from rest_framework import serializers
 from .models import Ride, RideEvent
 from users.models import User
+import datetime
 
 class UserRideSerializer(serializers.ModelSerializer):
     class Meta:
@@ -17,10 +18,15 @@ class RideSerializer(serializers.ModelSerializer):
     rider = UserRideSerializer(read_only=True)
     driver = UserRideSerializer(read_only=True)
     ride_events = GetRideEventSerializer(many=True, read_only=True)
+    todays_ride_events = serializers.SerializerMethodField()
     class Meta:
         model = Ride
         fields = '__all__'
-
+        
+    def get_todays_ride_events(self, obj):
+        todays_events = obj.ride_events.filter(created_at__date=datetime.date.today())
+        return GetRideEventSerializer(todays_events, many=True).data
+    
 class GetRideSerializer(serializers.ModelSerializer):
     class Meta:
         model = Ride
