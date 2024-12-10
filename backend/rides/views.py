9f143f9dcb1a2a57639526136342c238be637d6e
Clarence Baluyot <akingprincess@gmail.com>
2024-12-10 21:56:31 +0800

    Add Comment for IsAdmin Permission

--- backend/rides/views.py
+++ backend/rides/views.py
@@ -15,6 +15,7 @@ from .filters import RideFilter
 import logging
 logger = logging.getLogger(__name__)
 
+## Can be done using Manager to all Models. which will return None or empty list. when role is not admin. But i Just place it here for now
 class IsAdminUser(BasePermission):
     def has_permission(self, request, view):
         if request.user and request.user.is_authenticated:
