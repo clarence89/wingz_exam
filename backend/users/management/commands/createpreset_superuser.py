88f48fad37007a8df0cf8cd80e3c7f4d780ef133
Clarence Baluyot <akingprincess@gmail.com>
2024-12-11 03:10:41 +0800

    Change First Name and Last name of Preset Superuser

--- backend/users/management/commands/createpreset_superuser.py
+++ backend/users/management/commands/createpreset_superuser.py
@@ -12,8 +12,8 @@ class Command(BaseCommand):
         password = "adminpassword"
         if not User.objects.filter(username=username).exists():
             users = User.objects.create_superuser(username, email, password)
-            users.first_name = "Clarence"
-            users.last_name = "Baluyot"
+            users.first_name = "Admin"
+            users.last_name = "Account"
             users.role = Role.objects.get(name="admin")
             users.save()
             self.stdout.write(self.style.SUCCESS("Superuser created successfully."))
